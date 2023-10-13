83352d7b3774f11bddb3fa954a1ae1728764239e
mohamdikamelia <kamelia.mohamdi@laplateforme.io>
2023-10-13 16:53:44 +0200

    Job09 terminer

--- Job09/accessrights.sh
+++ Job09/accessrights.sh
@@ -1,27 +1,20 @@
 #!/bin/bash
+ 
+csv_file=/home/kamelia/Shell_Userlist.csv
 
-# Emplacement du fichier CSV
-csv_file="utilisateurs.csv"
+ while IFS=, read -r username role
 
-# Fonction pour créer un utilisateur
-create_user() {
-    username="$1"
-    password="$2"
-    admin="$3"
-
-    # Créer l'utilisateur
-    sudo useradd -m "$username"
-
-    # Définir le mot de passe
-    echo "$username:$password" | sudo chpasswd
-
-    # Si l'utilisateur est un admin, donner des privilèges sudo
-    if [ "$admin" == "admin" ]; then
-        sudo usermod -aG sudo "$username"
+do
+    if [ "$role" == "admin" ]; then
+        sudo useradd  sudo "$username"
+    else
+        sudo useradd  "$username"
     fi
-}
+    echo "L'utilisateur $username a le rôle $role."
 
-# Parcourir le fichier CSV et créer des utilisateurs
-awk -F, '{print $1, $2, $3}' "$csv_file" | while IFS= read -r line; do
-    create_user $line
-done
+done < "$csv_file"
+
+
+
+
+
